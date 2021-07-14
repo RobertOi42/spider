416d091dc60360653c0e45d68696265314049e00
RobertOi42 <87450722+RobertOi42@users.noreply.github.com>
2021-07-14 19:36:17 -0300

    Update spider.py

--- extra/extra/spiders/spider.py
+++ extra/extra/spiders/spider.py
@@ -8,12 +8,11 @@ class SpiderSpider(scrapy.Spider):
 	
 	name = 'spider'
 	allowed_domains = ['extra.com.br']
-	#start_im = 'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c56_c62&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1'	
-	#start_tv = 'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c1_c2&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1'
-	start = ['https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c13_c14_c13&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1',
-				'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c1_c2&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1',
-				'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c56_c62&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1']
-	start_urls = [start[0] % 1,start[1] % 1,start[2] % 1]
+	#urls para filtrar produtos a serem achados. Mudar manualmente para cada pois não consegui rodar todos em apenas uma compilação
+	#start = 'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c13_c14_c13&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1'
+	#start = 'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c1_c2&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1'
+	start = 'https://www.extra.com.br/api/catalogo-ssr/products/?Filtro=c56_c62&PaginaAtual=%s&RegistrosPorPagina=20&Platform=1'
+	start_urls = [start % 1]
 	download_delay = 1.5 
 	
 	custom_settings={ 'FEED_URI': "extra_%(name)s.csv",'FEED_FORMAT': 'csv'}       
@@ -25,6 +24,7 @@ class SpiderSpider(scrapy.Spider):
 		
 		data = json.loads(response.body)
 		
+		#número de produtos encontrado dividido pelo número mostrado por página, 20 neste caso
 		if c == 0:
 			t = data.get('filters',[])			
 			l = t[0].get('values')
@@ -33,13 +33,6 @@ class SpiderSpider(scrapy.Spider):
 		c1 = size//20
 		c1 += 1	
 			
-		#title = response.xpath("//div[@class='styles__ProductCardWrapper-sc-1gzprri-6 styles__ResponsiveWrapper-sc-1gzprri-9 hEMyDr']/div[@class='styles__Content-sc-1gzprri-7 jubJiu']/div[@class='styles__CardInfo-sc-1gzprri-5 iIqxUk']/a[@class='styles__Title-sc-1gzprri-1 kWIhVj']/text()").extract()
-		#url = response.xpath("//div[@class='styles__ProductCardWrapper-sc-1gzprri-6 styles__ResponsiveWrapper-sc-1gzprri-9 hEMyDr']/div[@class='styles__Content-sc-1gzprri-7 jubJiu']/div[@class='styles__CardInfo-sc-1gzprri-5 iIqxUk']/a/@href").extract()
-		#SKU = []
-		#for i in range(len(url)):	
-		#	u =[int(s) for s in url[i].split("/") if s.isdigit()]
-		#	SKU.append(u[0])
-		
 		
 		for item in data.get('products',[]):
 			yield {
@@ -48,12 +41,15 @@ class SpiderSpider(scrapy.Spider):
 				'url': item.get('urls')
 				}
 		
+		#passa para a próxima página do site
 		if c < c1:
 			next_page = c + 1
-			yield scrapy.Request(self.start[0] % next_page)
+			yield scrapy.Request(self.start % next_page)
 			
 			c += 1
 	
+		#dentro das categorias do site haviam erros, e por isso alguns produtos aparecem na lista sendo
+		#que não são do tipo dos produtos procurados	
 			
 		
 		
